e97590d9e865384938c49d27ab095761c664775b
pretteter <52958596+pretteter@users.noreply.github.com>
2022-01-30 12:42:39 +0100

    Create Addon.py

---
+++ Addon.py
@@ -0,0 +1,72 @@
+from random import random
+from bmesh.types import BMVert, BMesh
+import bpy
+import bmesh
+import math
+from bpy.types import Mesh, Object
+import mathutils
+import numpy as np
+from numpy.core.numeric import cross, outer
+
+
+def main(context):
+   
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    "Copy your Code here"
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+class SimpleOperator(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.simple_operator"
+    bl_label = "Generate Tree"
+
+
+    def execute(self, context):
+        main(context)
+        return {'FINISHED'}
+
+
+
+
+
+
+
+class LayoutDemoPanel(bpy.types.Panel):
+    """Creates a Panel in the scene context of the properties editor"""
+    bl_label = "Layout Demo"
+    bl_idname = "SCENE_PT_layout"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "scene"
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+
+        # Big render button
+        layout.label(text="Tree")
+        row = layout.row()
+        row.scale_y = 3.0
+        row.operator("object.simple_operator")
+
+
+
+def register():
+    bpy.utils.register_class(SimpleOperator)
+    bpy.utils.register_class(LayoutDemoPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(SimpleOperator)
+    bpy.utils.unregister_class(LayoutDemoPanel)
+
+
+if __name__ == "__main__":
+    register()
